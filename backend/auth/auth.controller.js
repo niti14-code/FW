7b187b54bdedef8046140d9f816d294e15209d48
Niti N Makam <nitimakam004@gmail.com>
2026-06-17 21:56:20 +0530

    Update for resetpassword

--- backend/auth/auth.controller.js
+++ backend/auth/auth.controller.js
@@ -203,11 +203,23 @@ if (role === 'admin' && adminKey !== 'freewheel') {
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
+/*const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  }
+});*/
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
