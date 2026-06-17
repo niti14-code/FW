4a5b622d0a5377e4b57d66e274bf67632bd90e14
Niti N Makam <nitimakam004@gmail.com>
2026-06-17 22:18:56 +0530

    Update using resend instead of nodemailer

--- backend/auth/auth.controller.js
+++ backend/auth/auth.controller.js
@@ -201,15 +201,12 @@ if (role === 'admin' && adminKey !== 'freewheel') {
 }
 */
 const crypto = require("crypto");
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+const resend = new Resend(
+  process.env.RESEND_API_KEY
+);
+/*const nodemailer = require("nodemailer");
 
-/*const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});*/
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
@@ -222,7 +219,7 @@ const transporter = nodemailer.createTransport({
     rejectUnauthorized: false
   }
 });
-
+*/
 exports.forgotPassword = async (req, res) => {
   try {
 
@@ -251,7 +248,22 @@ exports.forgotPassword = async (req, res) => {
       //`http://localhost:3000/reset-password/${resetToken}`;
     const resetUrl =
   `${process.env.FRONTEND_URL}?resetToken=${resetToken}`
-    await transporter.sendMail({
+    // Send reset email using Resend
+    await resend.emails.send({
+  from: "CampusRide <onboarding@resend.dev>",
+  to: email,
+  subject: "CampusRide - Request for Password Reset",
+  html: `
+    <h2>Password Reset</h2>
+    <p>You requested a password reset.</p>
+    <p>Click below:</p>
+
+    <a href="${resetUrl}">
+      Reset Password
+    </a>
+  `
+});
+    /*await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: email,
       subject: "CampusRide- Request for Password Reset",
@@ -263,7 +275,7 @@ exports.forgotPassword = async (req, res) => {
           Reset Password
         </a>
       `
-    });
+    });*/
 
     res.json({
       message: "Reset link sent"
